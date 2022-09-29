f0592839b1be82150b3c0c69d3614867fd0dcee1
CesarCruz-lab <rbcesar86@gmail.com>
2022-09-29 00:32:19 -0500

    3f03d26b-89a2-43c9-b533-7f3951ab9a8a

--- src/app.ts
+++ src/app.ts
@@ -37,6 +37,7 @@ export function createApplication() {
         await initializeDatabase()
         initializeMiddlawares()
 
+        console.log(uri)
         app.listen(uri.port)
       } catch (err: any) {
         console.error(err)
