c8cee0bb3914c002f548099e632f2beeb88fccfc
CesarCruz-lab <rbcesar86@gmail.com>
2022-10-06 00:15:13 -0500

    :sparkles: feat: CORS config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import morgan from 'morgan'
 
 import { uri } from '@app/config/access'
 import { router } from '@app/routes/routes'
 
 const app = express()
+const whitelist = uri.domains.split(',').map(item => item.trim())
 
 function createDatabaseInitializer() {
   return {
@@ -22,20 +25,37 @@ function createGlobalInitializer() {
       app.use(express.json())
       app.use(express.urlencoded({ extended: true }))
       app.use(cookieParser())
+      app.use(morgan('dev'))
       app.use(router)
+    },
+    initializeCors() {
+      app.use(
+        cors({
+          credentials: true,
+          origin: function (origin, cb) {
+            if (!origin || whitelist.indexOf(origin) !== -1) {
+              cb(null, true)
+              return
+            }
+
+            cb(new Error('Not allowed CORS'))
+          },
+        })
+      )
     },
   }
 }
 
 export function createApplication() {
   const { initializeDatabase } = createDatabaseInitializer()
-  const { initializeMiddlawares } = createGlobalInitializer()
+  const { initializeMiddlawares, initializeCors } = createGlobalInitializer()
 
   const application = {
     async start() {
       try {
         await initializeDatabase()
         initializeMiddlawares()
+        initializeCors()
 
         app.listen(uri.port)
       } catch (err: any) {
--- src/config/access.ts
+++ src/config/access.ts
@@ -1,6 +1,7 @@
 export const uri = {
   port: Number(process.env.PORT!),
   db_access: process.env.DB_ACCESS!,
+  domains: process.env.CORS_DOMAINS!,
   secret_access_key: process.env.SECRET_ACCESS_KEY!,
 
   aws: {
